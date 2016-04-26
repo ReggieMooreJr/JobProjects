8316ced89eb9d84c10aa44403cb2c64a1b222c37
ReggieMooreJr <mikodad1107@gmail.com>
2016-04-26 16:19:56 -0500

    Push 4
    updating script to maven project

--- src/UptakeCode/Homepage.java
+++ src/UptakeCode/Homepage.java
@@ -2,20 +2,40 @@ package UptakeCode;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 
+//// This class will identify all the objects on the home page for Uptake.com
 public class Homepage {
 
-	public Homepage() {
-		// TODO Auto-generated constructor stub
+	WebDriver driver;
+	
+	By MainClass = By.className("main");
+	By ApproachLink = By.id("menu-item-4164");
+	By PlatformLink = By.id("menu-item-4172");
+	By SolutionsLink = By.id("menu-item-4162");
+	By PeopleLink = By.id("menu-item-4162");
+	By JoinUsLink = By.id("menu-item-4167");
+	By BlogLink = By.id("menu-item-4166");
+	
+public Homepage(WebDriver driver){
+	this.driver=driver;
+}
+public void clickApproach(){
+
+	driver.findElement(ApproachLink).click();
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public void clickPlatformLink(){
+	driver.findElement(PlatformLink).click();
 	}
-
+public void clickSolutionsLink(){
+	driver.findElement(SolutionsLink).click();
+	}
+public void clickPeopleLink(){
+	driver.findElement(PeopleLink).click();
+	}
+public void clickJoinUsLink(){
+	driver.findElement(JoinUsLink).click();
+	}
+public void clickBlogLink(){
+	driver.findElement(BlogLink).click();
+	}
 }
