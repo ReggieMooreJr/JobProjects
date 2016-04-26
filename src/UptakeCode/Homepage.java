29241a637c270cbc7492f35e86b4dcc8f60ff606
ReggieMooreJr <mikodad1107@gmail.com>
2016-04-26 13:07:29 -0500

    Push2
    Adding the Uptake Project Round 1

---
+++ src/UptakeCode/Homepage.java
@@ -0,0 +1,21 @@
+package UptakeCode;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class Homepage {
+
+	public Homepage() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
